3fd675c400a261389646515a23453568c87ec6d0
B G GIRIDHARAN <b.g.giri2006@gmail.com>
2026-02-25 19:39:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/safe_walk/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
